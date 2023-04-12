822bdb1ff3119120a964c789d502f94f9e4b3112
csailer <chuck@chucksailer.com>
2023-04-12 11:09:53 -0400

    refactored downloading from s3

--- internal/s3/bucket.go
+++ internal/s3/bucket.go
@@ -1,20 +1,28 @@
 package s3
 
 import (
-	"errors"
+	"bufio"
+	"encoding/json"
 	"fmt"
-	"log"
+	"io"
+	"net/url"
 	"os"
-	"path"
-	"strings"
+	"path/filepath"
+	"regexp"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/chuxorg/chux-parser/config"
 )
 
+const basePath = "data/"
+
+// Define a struct to hold the JSON object's URL field
+type Line struct {
+	URL string `json:"url"`
+}
 type File struct {
 	Name         string
 	LastModified time.Time
@@ -39,7 +47,7 @@ type Bucket struct {
 var _cfg *config.ParserConfig
 
 func New(options ...func(*Bucket)) *Bucket {
-	
+
 	bucket := &Bucket{}
 	for _, option := range options {
 		option(bucket)
@@ -64,31 +72,6 @@ func newFile() *File {
 	return &File{}
 }
 
-// GetObjects retrieves the objects in an Amazon S3 bucket
-// Inputs:
-//
-//	sess is the current session, which provides configuration for the SDK's service clients
-//
-// Output:
-//
-//	If success, the list of objects and nil
-//	Otherwise, nil and an error from the call to ListObjectsV2
-func (b *Bucket) getObjects() (*s3.ListObjectsV2Output, error) {
-
-	svc := s3.New(b.Session)
-	if b.Name == "" {
-		b.logError("GetObjects: Bucket Name is not set")
-	}
-	// Get the list of items
-	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: &b.Name})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
-}
-
 // Logs an Error Message
 // Inputs:
 //
@@ -101,113 +84,168 @@ func (b Bucket) logError(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
 }
 
-// Starts an AWS Session if one does not
-// exist and sets the Session field of
-// the Bucket struct
-// Inputs:
-//
-//	None
-//
-// Output:
-//
-//	None
-func (b *Bucket) startSession() {
-	if b.Session != nil {
-		// Session already started
-		return
-	}
-	// create the session
-	var err error
-	// sess, err := session.NewSessionWithOptions(session.Options{
-	// })
-	sess, err := session.NewSession()
-	
+func (b *Bucket) Download() error {
+	// Replace with your bucket and region
+	s3Bucket := _cfg.AWS.BucketName
+	region := os.Getenv("AWS_REGION")
+
+	// Create a new AWS session with the specified region
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	}))
+
+	// Create a new S3 service instance using the session
+	svc := s3.New(sess)
+
+	// List objects in the S3 bucket
+	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(s3Bucket)})
 	if err != nil {
-		b.logError("GetSession: couldn't create an AWS session", err)
-	}
-	// assign the session to the Bucket
-	b.Session = sess
-}
-
-// Downloads all items in an s3 bucket.
-// The name of the Bucket is assigned to the Bucket Object
-// Returns an Slice of file names
-// Inputs:
-//
-//	None
-//
-// Output:
-//
-//	A slice of File structs that describes the items that were downloaded
-func (b *Bucket) DownloadAll() []File {
-
-	if b.Session == nil {
-		b.startSession()
+		fmt.Println("Error listing objects:", err)
+		return err
 	}
 
-	retVal := []File{}
-
-	resp, err := b.getObjects()
-	if err != nil {
-		b.logError("DownloadAll: Error from getObjects", err)
-	}
-	// go over the contents ...
+	// Iterate through each object in the bucket
 	for _, item := range resp.Contents {
-		// create a new File struct
-		file := newFile()
-		// set the fields of file
-		file.Name = *item.Key
-		file.LastModified = *item.LastModified
-		file.Size = *item.Size
-		file.IsProduct = !strings.Contains(*item.Key, "article")
-		// append to the slice of Files
-		retVal = append(retVal, *file)
-		// Download the file
-		// if the downloadpath doesn't exist, create the directory
-		if _, err := os.Stat(b.DownloadPath); errors.Is(err, os.ErrNotExist) {
-			err := os.Mkdir(b.DownloadPath, os.ModePerm)
-			if err != nil {
-				log.Println(err)
-			}
-		}
-		fileName := b.DownloadPath + "/" + file.Name
-		b.Download(&fileName)
-	}
+		objectKey := *item.Key
 
-	fmt.Println("Found", len(resp.Contents), "items in bucket", b.Name)
-	// return a slice of files that were downloaded
-	return retVal
+		// Download the object from S3
+		fileReader, err := svc.GetObject(&s3.GetObjectInput{
+			Bucket: aws.String(s3Bucket),
+			Key:    item.Key,
+		})
+		if err != nil {
+			fmt.Println("Error getting object:", err)
+			continue
+		}
+
+		// Read the first line (JSON object) from the file
+		lineReader := bufio.NewReader(fileReader.Body)
+		lineStr, err := lineReader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			fmt.Println("Error reading line:", err)
+			continue
+		}
+
+		// Unmarshal the JSON object into a Line struct
+		var lineObj Line
+		err = json.Unmarshal([]byte(lineStr), &lineObj)
+		if err != nil {
+			fmt.Println("Error un-marshalling JSON:", err)
+			continue
+		}
+
+		// Extract the FQDN from the URL
+		companyName, err := b.extractCompanyName(lineObj.URL)
+		if err != nil {
+			fmt.Println("Error extracting company name:", err)
+			continue
+		}
+
+		// Create the company directory if it doesn't exist
+		companyPath := filepath.Join(basePath, companyName)
+		err = os.MkdirAll(companyPath, os.ModePerm)
+		if err != nil {
+			fmt.Println("Error creating company directory:", err)
+			continue
+		}
+
+		// Create the subdir with date and time from the object key
+		dateTimeDir, err := b.formatDateDir(objectKey)
+		dateTimePath := filepath.Join(companyPath, dateTimeDir)
+		err = os.MkdirAll(dateTimePath, os.ModePerm)
+		if err != nil {
+			fmt.Println("Error creating date-time directory:", err)
+			continue
+		}
+
+		// Download the file to the newly created path
+		outputPath := filepath.Join(dateTimePath, companyName+".jl")
+		err = b.downloadFile(fileReader.Body, outputPath)
+		if err != nil {
+			fmt.Println("Error downloading file:", err)
+		}
+	}
 }
 
-// Downloads an Object from an s3 Bucket
-// Inputs:
-//
-//	objectName is the name of the Object to download from the Bucket
-//
-// Output:
-//
-//	Returns an error if one occurs. Otherwise, returns nil
-func (b *Bucket) Download(objectName *string) error {
-	fmt.Println("Downloading ", *objectName)
-	fileName := path.Base(*objectName)
-	file, err := os.Create(fileName)
-
+// The extractCompanyName function takes a raw URL string as input, parses it, and extracts the hostname.
+// It then uses a regular expression to remove the domain extension and any subdomains (e.g., "www").
+// The resulting company name is returned.
+func (b *Bucket) extractCompanyName(rawURL string) (string, error) {
+	// Parse the raw URL
+	parsedURL, err := url.Parse(rawURL)
 	if err != nil {
-		return err
+		return "", err
+	}
+	// Extract the hostname from the parsed URL
+	host := parsedURL.Hostname()
+
+	// Use a regular expression to match and remove the domain extension
+	// This regular expression matches the last segment of the domain name
+	// (e.g., "com", "edu") and any subdomains (e.g., "www")
+	re := regexp.MustCompile(`(?:\w+\.)?(\w+)\.\w+`)
+	matches := re.FindStringSubmatch(host)
+
+	// Check if the regular expression found a match
+	if len(matches) < 2 {
+		return "", fmt.Errorf("could not extract company name from URL")
 	}
 
-	defer file.Close()
+	// Return the matched company name (the second element in the matches slice)
+	return matches[1], nil
+}
 
-	downloader := s3manager.NewDownloader(b.Session)
+// formatDateDir takes an S3 object key string and extracts the date and time
+// from the key, formatting it as a directory name like "20230410-20.38.35"
+func (b *Bucket) formatDateDir(objectKey string) (string, error) {
+	// Use a regular expression to match the date and time from the object key
+	// This regular expression looks for a date and time pattern like "2023-04-10T20-38-35"
+	re := regexp.MustCompile(`(\d{4})-(\d{2})-(\d{2})T(\d{2})-(\d{2})-(\d{2})`)
+	matches := re.FindStringSubmatch(objectKey)
 
-	_, err = downloader.Download(file,
-		&s3.GetObjectInput{
-			Bucket: &b.Name,
-			Key:    objectName,
-		})
+	// Check if the regular expression found a match
+	if len(matches) < 7 {
+		return "", fmt.Errorf("could not extract date and time from object key")
+	}
 
+	// Extract the matched date and time components
+	year := matches[1]
+	month := matches[2]
+	day := matches[3]
+	hour := matches[4]
+	minute := matches[5]
+	second := matches[6]
+
+	// Format the date and time components as a directory name like "20230410-20.38.35"
+	dateTimeDir := fmt.Sprintf("%s%s%s-%s.%s.%s", year, month, day, hour, minute, second)
+
+	return dateTimeDir, nil
+}
+
+// downloadFile downloads the file from the S3 bucket, reads it from fileReader, and saves it to the specified outputPath.
+// Input:
+// - fileReader: An io.ReadCloser from which the file content will be read.
+// - outputPath: The path where the downloaded file will be saved.
+// Returns: An error if something goes wrong during the file download and save process.
+func (b *Bucket) downloadFile(fileReader io.ReadCloser, outputPath string) error {
+	// Ensure the fileReader is closed after the function finishes.
+	defer fileReader.Close()
+
+	// Create a new file or open an existing one at the outputPath.
+	// This will also truncate the file if it already exists, meaning
+	// it will clear the existing content and set the file size to zero.
+	outputFile, err := os.Create(outputPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create output file: %w", err)
+	}
+
+	// Ensure the outputFile is closed after the function finishes.
+	defer outputFile.Close()
+
+	// Copy the content from fileReader to outputFile.
+	// This will write the downloaded data from the S3 bucket into the file.
+	_, err = io.Copy(outputFile, fileReader)
+	if err != nil {
+		return fmt.Errorf("failed to copy data to output file: %w", err)
 	}
 
 	return nil
